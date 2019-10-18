17e0445e1420f8d12f38213d1057ed7c4d8f70dd
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-10-18 17:19:10 -0500

    Actualizacion Form "Peliculas"

--- Proyecto_Cine/Peliculas.Designer.vb
+++ Proyecto_Cine/Peliculas.Designer.vb
@@ -27,14 +27,19 @@ Partial Class Peliculas
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.txtTitulo = New System.Windows.Forms.TextBox()
+        Me.txtAño = New System.Windows.Forms.TextBox()
+        Me.cmbGenero = New System.Windows.Forms.ComboBox()
         Me.lblTitulo = New System.Windows.Forms.Label()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.cmbTanda = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.cmbTipo = New System.Windows.Forms.ComboBox()
         Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDuracion = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.cmbSala = New System.Windows.Forms.ComboBox()
+        Me.btnConfirmar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -73,29 +78,29 @@ Partial Class Peliculas
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Tanda "
         '
-        'TextBox1
+        'txtTitulo
         '
-        Me.TextBox1.Location = New System.Drawing.Point(182, 70)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 4
+        Me.txtTitulo.Location = New System.Drawing.Point(182, 70)
+        Me.txtTitulo.Name = "txtTitulo"
+        Me.txtTitulo.Size = New System.Drawing.Size(100, 20)
+        Me.txtTitulo.TabIndex = 4
         '
-        'TextBox2
+        'txtAño
         '
-        Me.TextBox2.Location = New System.Drawing.Point(182, 118)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 5
+        Me.txtAño.Location = New System.Drawing.Point(182, 122)
+        Me.txtAño.Name = "txtAño"
+        Me.txtAño.Size = New System.Drawing.Size(100, 20)
+        Me.txtAño.TabIndex = 5
         '
-        'ComboBox1
+        'cmbGenero
         '
-        Me.ComboBox1.DisplayMember = "Terror "
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Terror ", "Aventura ", "Romance "})
-        Me.ComboBox1.Location = New System.Drawing.Point(182, 171)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox1.TabIndex = 6
+        Me.cmbGenero.DisplayMember = "Terror "
+        Me.cmbGenero.FormattingEnabled = True
+        Me.cmbGenero.Items.AddRange(New Object() {"Terror ", "Aventura ", "Romance "})
+        Me.cmbGenero.Location = New System.Drawing.Point(182, 171)
+        Me.cmbGenero.Name = "cmbGenero"
+        Me.cmbGenero.Size = New System.Drawing.Size(121, 21)
+        Me.cmbGenero.TabIndex = 6
         '
         'lblTitulo
         '
@@ -106,13 +111,14 @@ Partial Class Peliculas
         Me.lblTitulo.TabIndex = 7
         Me.lblTitulo.Text = "Ingresar los datos de las películas" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
-        'ComboBox2
+        'cmbTanda
         '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(182, 222)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox2.TabIndex = 8
+        Me.cmbTanda.FormattingEnabled = True
+        Me.cmbTanda.Items.AddRange(New Object() {"Mañana", "Tarde", "Noche"})
+        Me.cmbTanda.Location = New System.Drawing.Point(182, 222)
+        Me.cmbTanda.Name = "cmbTanda"
+        Me.cmbTanda.Size = New System.Drawing.Size(121, 21)
+        Me.cmbTanda.TabIndex = 8
         '
         'Label5
         '
@@ -123,23 +129,67 @@ Partial Class Peliculas
         Me.Label5.TabIndex = 9
         Me.Label5.Text = "Tipo "
         '
-        'ComboBox3
+        'cmbTipo
         '
-        Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Items.AddRange(New Object() {"Regular ", "Estreno"})
-        Me.ComboBox3.Location = New System.Drawing.Point(182, 268)
-        Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox3.TabIndex = 10
+        Me.cmbTipo.FormattingEnabled = True
+        Me.cmbTipo.Items.AddRange(New Object() {"Regular ", "Estreno"})
+        Me.cmbTipo.Location = New System.Drawing.Point(182, 268)
+        Me.cmbTipo.Name = "cmbTipo"
+        Me.cmbTipo.Size = New System.Drawing.Size(121, 21)
+        Me.cmbTipo.TabIndex = 10
         '
         'btnIngresar
         '
-        Me.btnIngresar.Location = New System.Drawing.Point(149, 311)
+        Me.btnIngresar.Location = New System.Drawing.Point(92, 424)
         Me.btnIngresar.Name = "btnIngresar"
         Me.btnIngresar.Size = New System.Drawing.Size(75, 23)
         Me.btnIngresar.TabIndex = 11
         Me.btnIngresar.Text = "Ingresar"
         Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(103, 318)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Duracion"
+        '
+        'txtDuracion
+        '
+        Me.txtDuracion.Location = New System.Drawing.Point(182, 315)
+        Me.txtDuracion.Name = "txtDuracion"
+        Me.txtDuracion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDuracion.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(103, 366)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Sala"
+        '
+        'cmbSala
+        '
+        Me.cmbSala.FormattingEnabled = True
+        Me.cmbSala.Items.AddRange(New Object() {"1", "2", "3", "4"})
+        Me.cmbSala.Location = New System.Drawing.Point(182, 363)
+        Me.cmbSala.Name = "cmbSala"
+        Me.cmbSala.Size = New System.Drawing.Size(121, 21)
+        Me.cmbSala.TabIndex = 19
+        '
+        'btnConfirmar
+        '
+        Me.btnConfirmar.Enabled = False
+        Me.btnConfirmar.Location = New System.Drawing.Point(228, 423)
+        Me.btnConfirmar.Name = "btnConfirmar"
+        Me.btnConfirmar.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirmar.TabIndex = 20
+        Me.btnConfirmar.Text = "Confirmar"
+        Me.btnConfirmar.UseVisualStyleBackColor = True
         '
         'Peliculas
         '
@@ -147,19 +197,25 @@ Partial Class Peliculas
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(390, 346)
+        Me.ClientSize = New System.Drawing.Size(390, 472)
+        Me.Controls.Add(Me.btnConfirmar)
+        Me.Controls.Add(Me.cmbSala)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtDuracion)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.btnIngresar)
-        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.cmbTipo)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.cmbTanda)
         Me.Controls.Add(Me.lblTitulo)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cmbGenero)
+        Me.Controls.Add(Me.txtAño)
+        Me.Controls.Add(Me.txtTitulo)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Peliculas"
         Me.Text = "Peliculas"
         Me.ResumeLayout(False)
@@ -171,12 +227,17 @@ Partial Class Peliculas
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents txtTitulo As TextBox
+    Friend WithEvents txtAño As TextBox
+    Friend WithEvents cmbGenero As ComboBox
     Friend WithEvents lblTitulo As Label
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents cmbTanda As ComboBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents cmbTipo As ComboBox
     Friend WithEvents btnIngresar As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtDuracion As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents cmbSala As ComboBox
+    Friend WithEvents btnConfirmar As Button
 End Class
--- Proyecto_Cine/Peliculas.vb
+++ Proyecto_Cine/Peliculas.vb
@@ -1,3 +1,32 @@
 ﻿Public Class Peliculas
+    Dim titulo(2) As String
+    Dim genero(2) As String
+    Dim tanda(2) As String
+    Dim tipo(2) As String
+    Dim año(2) As String
+    Dim duracion(2) As String
+    Dim sala(2) As Integer
+    Dim cont As Integer
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
 
+        If (cont = 2) Then
+            btnIngresar.Enabled = False
+            btnConfirmar.Enabled = True
+        End If
+
+        titulo(cont) = txtTitulo.Text
+        año(cont) = txtAño.Text
+        genero(cont) = cmbGenero.Text
+        tanda(cont) = cmbTanda.Text
+        tipo(cont) = cmbTipo.Text
+        duracion(cont) = txtDuracion.Text
+        sala(cont) = cmbSala.Text
+
+        cont = cont + 1
+
+    End Sub
+
+    Private Sub BtnConfirmar_Click(sender As Object, e As EventArgs) Handles btnConfirmar.Click
+        Inicio.Show()
+    End Sub
 End Class
