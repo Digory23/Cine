743df62b71b0f9357beaa45e46069c00538af941
Diana <dianac.vergara1@gmail.com>
2019-10-19 17:21:01 -0500

    Formulario Factura

--- Proyecto_Cine/Factura.Designer.vb
+++ Proyecto_Cine/Factura.Designer.vb
@@ -22,9 +22,288 @@ Partial Class Factura
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.gbxDetalle = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtAsientos = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtNomCliente = New System.Windows.Forms.TextBox()
+        Me.txtNomPeli = New System.Windows.Forms.TextBox()
+        Me.txtNiños = New System.Windows.Forms.TextBox()
+        Me.txtAdultos = New System.Windows.Forms.TextBox()
+        Me.txtSala = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnRegresar = New System.Windows.Forms.Button()
+        Me.gbxDetalle.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gbxDetalle
+        '
+        Me.gbxDetalle.Controls.Add(Me.txtSala)
+        Me.gbxDetalle.Controls.Add(Me.txtAdultos)
+        Me.gbxDetalle.Controls.Add(Me.txtNiños)
+        Me.gbxDetalle.Controls.Add(Me.txtNomPeli)
+        Me.gbxDetalle.Controls.Add(Me.txtNomCliente)
+        Me.gbxDetalle.Controls.Add(Me.txtAsientos)
+        Me.gbxDetalle.Controls.Add(Me.Label8)
+        Me.gbxDetalle.Controls.Add(Me.Label7)
+        Me.gbxDetalle.Controls.Add(Me.Label6)
+        Me.gbxDetalle.Controls.Add(Me.Label4)
+        Me.gbxDetalle.Controls.Add(Me.DateTimePicker1)
+        Me.gbxDetalle.Controls.Add(Me.Label3)
+        Me.gbxDetalle.Controls.Add(Me.Label2)
+        Me.gbxDetalle.Controls.Add(Me.Label1)
+        Me.gbxDetalle.Location = New System.Drawing.Point(22, 26)
+        Me.gbxDetalle.Name = "gbxDetalle"
+        Me.gbxDetalle.Size = New System.Drawing.Size(315, 245)
+        Me.gbxDetalle.TabIndex = 0
+        Me.gbxDetalle.TabStop = False
+        Me.gbxDetalle.Text = "Detalle del Consumo"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cliente: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Película: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Fecha: "
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(83, 97)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(196, 20)
+        Me.DateTimePicker1.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(120, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "No. Sala: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(63, 132)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Niños:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(166, 132)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Adultos: "
+        '
+        'txtAsientos
+        '
+        Me.txtAsientos.Location = New System.Drawing.Point(90, 188)
+        Me.txtAsientos.Name = "txtAsientos"
+        Me.txtAsientos.ReadOnly = True
+        Me.txtAsientos.Size = New System.Drawing.Size(189, 20)
+        Me.txtAsientos.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(32, 191)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(59, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Asiento(s): "
+        '
+        'txtNomCliente
+        '
+        Me.txtNomCliente.Location = New System.Drawing.Point(82, 36)
+        Me.txtNomCliente.Name = "txtNomCliente"
+        Me.txtNomCliente.ReadOnly = True
+        Me.txtNomCliente.Size = New System.Drawing.Size(189, 20)
+        Me.txtNomCliente.TabIndex = 16
+        '
+        'txtNomPeli
+        '
+        Me.txtNomPeli.Location = New System.Drawing.Point(82, 66)
+        Me.txtNomPeli.Name = "txtNomPeli"
+        Me.txtNomPeli.ReadOnly = True
+        Me.txtNomPeli.Size = New System.Drawing.Size(189, 20)
+        Me.txtNomPeli.TabIndex = 17
+        '
+        'txtNiños
+        '
+        Me.txtNiños.Location = New System.Drawing.Point(106, 129)
+        Me.txtNiños.Name = "txtNiños"
+        Me.txtNiños.ReadOnly = True
+        Me.txtNiños.Size = New System.Drawing.Size(38, 20)
+        Me.txtNiños.TabIndex = 18
+        '
+        'txtAdultos
+        '
+        Me.txtAdultos.Location = New System.Drawing.Point(220, 129)
+        Me.txtAdultos.Name = "txtAdultos"
+        Me.txtAdultos.ReadOnly = True
+        Me.txtAdultos.Size = New System.Drawing.Size(38, 20)
+        Me.txtAdultos.TabIndex = 19
+        '
+        'txtSala
+        '
+        Me.txtSala.Location = New System.Drawing.Point(171, 158)
+        Me.txtSala.Name = "txtSala"
+        Me.txtSala.ReadOnly = True
+        Me.txtSala.Size = New System.Drawing.Size(38, 20)
+        Me.txtSala.TabIndex = 20
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(39, 277)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(287, 131)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Total a Pagar"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(60, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(79, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Monto a pagar:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(142, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(78, 20)
+        Me.TextBox1.TabIndex = 21
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(59, 61)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(80, 13)
+        Me.Label9.TabIndex = 22
+        Me.Label9.Text = "Monto Pagado:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(142, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(78, 20)
+        Me.TextBox2.TabIndex = 23
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(142, 91)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(78, 20)
+        Me.TextBox3.TabIndex = 25
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(59, 94)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 13)
+        Me.Label10.TabIndex = 24
+        Me.Label10.Text = "Monto Devuelto:"
+        '
+        'btnRegresar
+        '
+        Me.btnRegresar.Location = New System.Drawing.Point(112, 428)
+        Me.btnRegresar.Name = "btnRegresar"
+        Me.btnRegresar.Size = New System.Drawing.Size(135, 22)
+        Me.btnRegresar.TabIndex = 2
+        Me.btnRegresar.Text = "Regresar"
+        Me.btnRegresar.UseVisualStyleBackColor = True
+        '
+        'Factura
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(359, 471)
+        Me.Controls.Add(Me.btnRegresar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.gbxDetalle)
+        Me.Name = "Factura"
         Me.Text = "Factura"
+        Me.gbxDetalle.ResumeLayout(False)
+        Me.gbxDetalle.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents gbxDetalle As GroupBox
+    Friend WithEvents txtSala As TextBox
+    Friend WithEvents txtAdultos As TextBox
+    Friend WithEvents txtNiños As TextBox
+    Friend WithEvents txtNomPeli As TextBox
+    Friend WithEvents txtNomCliente As TextBox
+    Friend WithEvents txtAsientos As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnRegresar As Button
 End Class
