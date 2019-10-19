c6ce052e494ac6ef11ad17dd4de0ff76c329fd11
LordDigory <54564313+LordDigory@users.noreply.github.com>
2019-10-19 18:47:58 -0500

    Se añadio un modulo para controlar los datos

--- Proyecto_Cine/Inicio.vb
+++ Proyecto_Cine/Inicio.vb
@@ -1,17 +1,5 @@
 ﻿Public Class Inicio 'En este formulario se muestra el menu principal
 
-    Dim peli(2) As pelicula 'Arreglo donde se almacenaran los datos de la pelicula
-
-
-    Public Sub Cargar(tit As String, gen As String, tanda As String, tipo As String, año As String, duracion As String, sala As Char, cont As Integer)
-        peli(cont).titulo = tit
-        peli(cont).genero = gen
-        peli(cont).tanda = tanda
-        peli(cont).tipo = tipo
-        peli(cont).año = año
-        peli(cont).duracion = duracion
-        peli(cont).sala = sala
-    End Sub
 
 
 
@@ -22,7 +10,6 @@
     End Sub
 
     Private Sub BtnConsultar_Click(sender As Object, e As EventArgs) Handles btnConsultar.Click
-        MsgBox(peli(1).titulo)
 
         Hide()
     End Sub
@@ -31,8 +18,5 @@
 
     End Sub
 
-    Structure pelicula
-        Dim titulo, genero, tanda, tipo, año, duracion As String
-        Dim sala As Char
-    End Structure
+
 End Class
