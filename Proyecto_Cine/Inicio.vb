1078807fb7a764214b1348532b46fe146cfba62b
virgilio-v <virgilio-v@users.github.com>
2019-10-20 15:12:48 -0500

    Totales
    Cambios en apariencia de formulario de Totales e inclusion de opciones de Salir y ver Totales en Inicio

--- Proyecto_Cine/Inicio.vb
+++ Proyecto_Cine/Inicio.vb
@@ -15,8 +15,14 @@
     End Sub
 
     Private Sub BtnRevisar_Click(sender As Object, e As EventArgs) Handles btnRevisar.Click
-
+        Asientos.Show()
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Totales.Show()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
 End Class
